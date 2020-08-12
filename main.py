c6c8227ab3450a72a72ad7bfe68cb74afb6007b5
Syntex2k <fabian.georgi@ymail.com>
2020-08-12 19:18:27 +0200

    started documenting, outsourced the main.py file

--- main.py
+++ main.py
@@ -2,39 +2,39 @@ import os
 
 import neat
 
-from game import eval_genomes
+from game import evaluate_genomes
 
 
 def run(config_file):
-    """
-    runs the NEAT algorithm to train a neural network to play flappy bird.
-    :param config_file: location of config file
-    :return: None
-    """
+    # Load in the config file for the neat architecture
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         config_file)
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_file)
 
-    # Create the population, which is the top-level object for a NEAT run.
-    p = neat.Population(config)
+    # Create a population out of the provided config
+    population = neat.Population(config)
 
-    # Add a stdout reporter to show progress in the terminal.
-    p.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    p.add_reporter(stats)
-    #p.add_reporter(neat.Checkpointer(5))
+    # Add so-called reporter. They enable to print out statistics in the console allowing for deeper analysis
+    population.add_reporter(neat.StdOutReporter(True))
+    population.add_reporter(neat.StatisticsReporter())
 
-    # Run for up to 50 generations.
-    winner = p.run(eval_genomes, 2)
+    # We let the population run. In order to do this, we need to provide a fitness-function which takes two
+    # arguments:
+    #   1. The population as a list of (genome_id, genome) tuples.
+    #   2. The current configuration object.
+    # In this function the whole logic of the game takes place. It does contain the game loop and assigns each bird
+    # their fitness score.
+    # We let the simulation run until there are 50 generations or a bird in a previous generation does so well that
+    # it does not hit a pole anymore, so we can stop there.
+    winner = population.run(evaluate_genomes, 50)
 
-    # show final stats
+    # Print out the stats (genomes, with their weights and connections) of the best bird
     print('\nBest genome:\n{!s}'.format(winner))
 
 
 if __name__ == '__main__':
-    # Determine path to configuration file. This path manipulation is
-    # here so that the script will run successfully regardless of the
-    # current working directory.
+    # Concatenate the paths so we do not get problems with different Operating Systems and working directories.
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    # Starts the game by creating a neat population and running the simulation
     run(config_path)
